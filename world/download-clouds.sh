1c8b663bab6205fe3bf04bf5ef7bd17ca0fc3ce5
chrono <chrono@open-resource.org>
2014-06-16 11:56:01 +0000

    Added new cloudmap repo as default URL

--- world/download-clouds.sh
+++ world/download-clouds.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-wget --no-cache http://xplanetclouds.com/free/coral/clouds_2048.jpg
-convert -resize 5400x2700 clouds_2048.jpg clouds.jpg
-rm clouds_2048.jpg
+wget --no-cache https://github.com/apollo-ng/cloudmap/blob/master/global.jpg 
+cp global.jpg clouds.jpg
+rm global.jpg
+#convert -resize 5400x2700 clouds_2048.jpg clouds.jpg
+#rm clouds_2048.jpg
