6aa898d52d62da765af31203f0b7081b8aa2906e
chrono <chrono@open-resource.org>
2014-07-01 13:42:04 +0000

    Added geostationary TLE

--- satellites/download-tle.sh
+++ satellites/download-tle.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# ISS
+# ISS (LEO) ####################################################################
 
 wget http://www.celestrak.com/NORAD/elements/stations.txt --no-cache -q -O stations.txt > /dev/null 2>&1
 
 if [ -s stations.txt ];
 then
     dos2unix stations.txt > /dev/null 2>&1
-    echo "$(cat stations.txt | grep -A2 ISS | tail -1 | awk '{print $2;}') \"  ISS\" image=satellites/iss.png transparent={0,0,0} color={255,255,255} altcirc=0 altcirc=45 trail={orbit,-15,5,0.1}" > iss
+    echo "$(cat stations.txt | grep -A2 ISS | tail -1 | awk '{print $2;}') \"  ISS\" image=satellites/iss.png transparent={0,0,0} color={255,255,255} altcirc=0 altcirc=45 trail={orbit,-15,5,1}" > iss
     cat stations.txt | grep --no-group-separator -A2 ISS > iss.tle
     rm stations.txt
 fi
 
-# Iridium
+# Iridium (LEO) ################################################################
 
 wget http://www.celestrak.com/NORAD/elements/iridium.txt --no-cache -q -O iridium.txt > /dev/null 2>&1
 
@@ -21,6 +21,7 @@ then
     dos2unix iridium.txt > /dev/null 2>&1
     mv iridium.txt iridium.tle
     cat iridium.tle | grep IRIDIUM > .iridium.tmp
+    echo "" > iridium
     while read in;
     do
         SAT=$(cat iridium.tle | grep --no-group-separator -A2 "${in:0:10}" | tail -1 | awk '{print $2;}' )
@@ -30,7 +31,7 @@ then
 fi
 
 
-# Classified/Spy/Surveilance/Military subset
+# Classified/Spy/Surveilance/Military (mostly LEO) #############################
 
 get_sats_by_name () {
 
@@ -71,5 +72,24 @@ then
     rm classfd.*
 
 fi
+
+# Geostationary (GEO) ##########################################################
+
+wget http://www.celestrak.com/NORAD/elements/geo.txt --no-cache -q -O geo.txt > /dev/null 2>&1
+
+if [ -s geo.txt ];
+then
+    dos2unix geo.txt > /dev/null 2>&1
+    mv geo.txt geo.tle
+    awk '{if (count++%3==0) print $0;}' geo.tle > .geo.tmp
+    echo "" > geo
+    while read in;
+    do
+        SAT=$(cat geo.tle | grep --no-group-separator -A2 "${in}" | tail -1 | awk '{print $2;}' )
+        echo "${SAT} \"  ${in}\" image=satellites/sat.png transparent={0,0,0} color={117,137,12} fontsize=9 trail={orbit,-5,0,1}" >> geo
+    done < .geo.tmp
+    rm .geo.tmp
+fi
+
 
 echo "$(date +%Y%m%d-%h%m)" > .last_updated
